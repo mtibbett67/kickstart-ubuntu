c506f75f990a259623bfb1d802342383894d0563
Mark Tibbett <mtibbett67@gmail.com>
2025-04-08 21:57:21 -0600

    Updated README.md

--- post-ubuntu.sh
+++ post-ubuntu.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# --- Install Packages ---
-PACKAGES="net-tools zsh neovim git"
-echo "Installing packages: $PACKAGES"
-sudo apt install -y $PACKAGES
-echo "Packages installed."
-
-# Set Zsh as default shell for new user
-echo "Changing default shell to Zsh for "$USERNAME"..."
-chsh -s "$(which zsh)" "$USERNAME"
-echo "Default shell for $USERNAME changed to Zsh."
-
 echo "Creating Documents directory..."
 mkdir -p ~/Documents
 cd ~/Documents
@@ -23,10 +12,8 @@ git clone "$DOTFILES_REPO"
 echo "Dotfiles cloned."
 
 # --- Copy Zsh Configuration ---
-ZSHRC_SOURCE="~/Documents/dotfiles/zsh/.zshrc"
-ZSHRC_DEST="~/"
 echo "Copying Zsh configuration from '$ZSHRC_SOURCE' to '$ZSHRC_DEST'..."
-cp "$ZSHRC_SOURCE" "$ZSHRC_DEST"
+cp ~/Documents/dotfiles/zsh/.zshrc ~/
 source ~/.zshrc
 echo "Zsh configuration copied."
 
