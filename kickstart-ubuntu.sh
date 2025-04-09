f2b3db914af21de1f03a3d58ecee858c84d440ba
Mark Tibbett <mtibbett67@gmail.com>
2025-04-08 20:43:57 -0600

    Split scripts into kickstart and post

--- kickstart-ubuntu.sh
+++ kickstart-ubuntu.sh
@@ -25,11 +25,6 @@ echo "Adding user $USERNAME to the sudo group..."
 usermod -aG sudo "$USERNAME"
 echo "User '$USERNAME' added successfully."
 
-# --- System Updates and Upgrade ---
-echo "Running system updates and upgrading to the newest distribution..."
-apt update && apt upgrade -y && do-release-upgrade
-echo "System update and upgrade complete."
-
 # --- Install Packages ---
 PACKAGES="net-tools zsh neovim git"
 echo "Installing packages: $PACKAGES"
@@ -41,31 +36,10 @@ echo "Changing default shell to Zsh for "$USERNAME"..."
 chsh -s "$(which zsh)" "$USERNAME"
 echo "Default shell for $USERNAME changed to Zsh."
 
-# --- Switch to New User and Configure ---
-echo "Switching to user: $USERNAME"
-sudo su - "$USERNAME"
-
-echo "Creating Documents directory..."
-mkdir -p ~/Documents
-cd ~/Documents
-echo "Current directory: $(pwd)"
-
-# --- Clone Dotfiles ---
-DOTFILES_REPO="https://github.com/mtibbett67/dotfiles.git"
-echo "Cloning dotfiles from: $DOTFILES_REPO"
-git clone "$DOTFILES_REPO"
-echo "Dotfiles cloned."
-
-# --- Copy Zsh Configuration ---
-ZSHRC_SOURCE="~/Documents/dotfiles/zsh/.zshrc"
-ZSHRC_DEST="~/.zshrc"
-echo "Copying Zsh configuration from '$ZSHRC_SOURCE' to '$ZSHRC_DEST'..."
-cp "$ZSHRC_SOURCE" "$ZSHRC_DEST"
-echo "Zsh configuration copied."
-
-# --- Get System IP Address ---
-echo "Getting system IP address..."
-ip a | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | cut -d'/' -f1
-echo "IP address displayed."
+# --- System Updates and Upgrade ---
+echo "Running system updates and upgrading to the newest distribution..."
+echo "After reboot login a new user and wget post-ubuntu.sh and run."
+apt update && apt upgrade -y && do-release-upgrade
+echo "System update and upgrade complete."
 
 echo "Script execution complete."
---
+++ post-ubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Creating Documents directory..."
+mkdir -p ~/Documents
+cd ~/Documents
+echo "Current directory: $(pwd)"
+
+# --- Clone Dotfiles ---
+DOTFILES_REPO="https://github.com/mtibbett67/dotfiles.git"
+echo "Cloning dotfiles from: $DOTFILES_REPO"
+git clone "$DOTFILES_REPO"
+echo "Dotfiles cloned."
+
+# --- Copy Zsh Configuration ---
+ZSHRC_SOURCE="~/Documents/dotfiles/zsh/.zshrc"
+ZSHRC_DEST="~/.zshrc"
+echo "Copying Zsh configuration from '$ZSHRC_SOURCE' to '$ZSHRC_DEST'..."
+cp "$ZSHRC_SOURCE" "$ZSHRC_DEST"
+echo "Zsh configuration copied."
+
+# --- Get System IP Address ---
+echo "Getting system IP address..."
+ip a | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | cut -d'/' -f1
+echo "IP address displayed."
+
+echo "Script execution complete."
